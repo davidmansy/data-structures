fcae931443e18aa827dc64fec21bff27384d35f0
Fred Zirdung <fred@hackreactor.com>
2013-12-21 15:33:45 -0800

    Initial commit

---
+++ sprint-one/src/functional/stack.js
@@ -0,0 +1,21 @@
+var makeStack = function(){
+    var instance = {};
+
+  // Use an object with numeric keys to store values
+    var storage = {};
+  /* START PROMPT
+    var size; // Hint: set an initial value here
+  -END PROMPT */
+
+  // Implement the methods below
+  instance.push = function(value){
+  };
+
+  instance.pop = function(){
+  };
+
+  instance.size = function(){
+  };
+  
+    return instance;
+  };
