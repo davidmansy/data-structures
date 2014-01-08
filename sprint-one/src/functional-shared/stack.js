0aa07c2a7b54bfac3c4b24e66b60c9504b626fc4
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 15:35:10 -0800

    Fills in functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,28 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+  instance.storage = {};
+  instance.sizeValue = 0;
+  _.extend(instance, stackMethods);
+  return instance;
 };
-  
-var stackMethods = {};
+
+var stackMethods = {
+  push : function(value){
+    this.storage[this.sizeValue++] = value;
+  },
+
+  pop : function(){
+    if(this.sizeValue) {
+      var val = this.storage[this.sizeValue-1];
+      delete this.storage[this.sizeValue-1];
+      this.sizeValue--;
+      return val;
+    }
+  },
+
+  size : function(){
+    return this.sizeValue;
+  }
+
+};
