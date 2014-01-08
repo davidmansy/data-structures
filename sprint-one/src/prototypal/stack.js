b7c59ceb17e39e4f6e0ec2284e8089e9461a53a6
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 15:46:51 -0800

    Fills in prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,26 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.sizeValue = 0;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value){
+    this.storage[this.sizeValue++] = value;
+  },
+
+  pop : function(){
+    if(this.sizeValue) {
+      var val = this.storage[this.sizeValue-1];
+      delete this.storage[this.sizeValue-1];
+      this.sizeValue--;
+      return val;
+    }
+  },
+
+  size : function(){
+    return this.sizeValue;
+  }
+
+};
