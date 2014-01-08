ac1a84ddc711463146c896968ff0bb9007272ca3
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 15:49:40 -0800

    Fills in prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,31 @@
 var makeQueue = function() {
   // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var instance = Object.create(queueMethods);
+  instance.storage = {};
+  instance.start = 0;
+  instance.end = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue : function(value){
+    this.storage[this.end] = value;
+    this.end++;
+  },
+
+  dequeue : function(){
+    if (this.size() > 0) {
+      var result = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      return result;
+    }
+  },
+
+  size : function(){
+    return this.end - this.start;
+  }
+
+};
