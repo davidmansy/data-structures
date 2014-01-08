28c82d4ff3be3d6c46daf3311006611640dabcf0
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 15:40:53 -0800

    Fills in functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,36 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  instance.storage = {};
+  instance.start = 0;
+  instance.end = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue : function(value){
+    this.storage[this.end] = value;
+    this.end++;
+  },
+
+  dequeue : function(){
+    if (this.size() > 0) {
+      var result = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      return result;
+    }
+  },
+
+  size : function(){
+    return this.end - this.start;
+  }
+};
+
+
+
+
