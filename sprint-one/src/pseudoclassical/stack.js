402bd3e86eaf388b98f58412f014786d997ed4c1
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 16:04:22 -0800

    Filled in pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.sizeValue = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.sizeValue++] = value;
+};
+
+Stack.prototype.pop = function(){
+  if(this.sizeValue) {
+    var val = this.storage[this.sizeValue-1];
+    delete this.storage[this.sizeValue-1];
+    this.sizeValue--;
+    return val;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.sizeValue;
+};
+
