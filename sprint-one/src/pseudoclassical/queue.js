f070e8145b16c487a42fb4d9e2cfdaa0f426670e
Hack Reactor Students <david.mansy@icloud.com>
2014-01-08 16:10:39 -0800

    Fills in pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,24 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.end] = value;
+  this.end++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.size() > 0) {
+    var result = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return result;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.end - this.start;
 };
