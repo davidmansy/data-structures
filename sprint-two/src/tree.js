d90a6afaad44225481e7342550fcaaee69e27156
Hack Reactor Students <david.mansy@icloud.com>
2014-01-09 14:26:07 -0800

    Fills in addChild for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -10,6 +10,10 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var newTree = makeTree(value);
+
+  this.children = this.children || []; //check/initalize children of tree
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target){
