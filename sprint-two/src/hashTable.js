077a6075953f62636df1c4f7935477062575a776
Hack Reactor Students <david.mansy@icloud.com>
2014-01-09 20:00:43 -0800

    Refactors code to create getHash helper function

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,22 +4,26 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = this.getHash(k);
   this._storage.set(i, v);
 };
 
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = this.getHash(k);
   return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = this.getHash(k);
   this._storage.each(function(value, key, storage) {
     if(key === i) {
       storage[key] = null;
     }
   });
+};
+
+HashTable.prototype.getHash = function(k) {
+  return getIndexBelowMaxForKey(k, this._limit);
 };
 
