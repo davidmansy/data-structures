112950c335cd3c81d0d2cd3baec27ce65db447ed
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 15:41:37 -0800

    Filled in insert/retrieve functions for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,12 +5,27 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-
+  var i = this.getHash(k);
+  if(this._storage.get(i) === undefined) {
+    this._storage.set(i, [[k,v]]);  //initialize array bucket with key-value array item
+  } else {
+    this._storage.get(i).push([k,v]);
+  }
 };
 
 
 HashTable.prototype.retrieve = function(k){
+  var i = this.getHash(k);
+  var val = null;
 
+  //retrieve array
+  var bucket = this._storage.get(i);
+  _.each(bucket, function(keyValuePair) {  //key stored at [0], val stored at [1]
+    if(keyValuePair[0] === k) {
+      val = keyValuePair[1];
+    }
+  });
+  return val;
 };
 
 HashTable.prototype.remove = function(k){
