2b5b267aa24bfe23f9659a15904ec1d645060920
Hack Reactor Students <david.mansy@icloud.com>
2014-01-09 16:41:56 -0800

    Fills in set; passes initial specs

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,24 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  var key = JSON.stringify(item);
+  if(!(key in this._storage)) {
+    this._storage[key] = item;
+  }
 };
 
 setPrototype.contains = function(item){
+  var key = JSON.stringify(item);
+  return (key in this._storage);
 };
 
 setPrototype.remove = function(item){
+  var key = JSON.stringify(item);
+  delete this._storage[key];
 };
