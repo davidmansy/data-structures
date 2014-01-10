0584c124f32f59072532723122d2e3600743d97f
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 15:29:09 -0800

    Reset hashTable.js for re-implementation that handles collision conflicts

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,39 +6,15 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
 
-  if(this._currentSize+1 > (this._limit * 0.75)) {  //check before maxing out storage
-
-    var oldStorage =  this._storage; //save old values
-    this._limit *= 2; //double the size limit
-    this._storage = makeLimitedArray(this._limit); //create new empty storage with double the size
-    var context = this;
-    this._currentSize = 0; //reset currentSize to zero --> no vals in storage
-    oldStorage.each(function(val, key) {
-      context.insert(/*We need to find a way to recreate hash values for larger storage*/);
-    });
-
-
-  }
-
-  this._currentSize++;
-  var i = this.getHash(k);
-  this._storage.set(i, v);
 };
 
 
 HashTable.prototype.retrieve = function(k){
-  var i = this.getHash(k);
-  return this._storage.get(i);
+
 };
 
 HashTable.prototype.remove = function(k){
-  var i = this.getHash(k);
-  this._storage.each(function(value, key, storage) {
-    if(key === i) {
-      storage[key] = null;
-    }
-  });
-  this._currentSize--;
+
 };
 
 HashTable.prototype.getHash = function(k) {
