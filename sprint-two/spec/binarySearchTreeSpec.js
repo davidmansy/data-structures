3913e4f2e2f4d2b80257c75c2a65454bfb5ee736
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 21:45:52 -0800

    Adds test to print array to check that all values have been visited

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -45,4 +45,18 @@ describe("binarySearchTree", function() {
     binarySearchTree.depthFirstLog(func);
     assert.notStrictEqual(array, [5,2,3]);
   });
+
+  it("Print array, no test", function(){
+    var array = [];
+    var func = function(value){ array.push(value); };
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(5);
+    binarySearchTree.insert(4);
+    binarySearchTree.depthFirstLog(func);
+    console.log(array);
+  });
 });
