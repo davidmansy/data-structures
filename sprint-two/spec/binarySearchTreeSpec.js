1cdf75501f3a270df669616aed61a61f79aaf0f9
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 20:47:03 -0800

    Adds tests to check insert for tree with only one level

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -13,6 +13,13 @@ describe("binarySearchTree", function() {
     expect(binarySearchTree.depthFirstLog).to.be.a('function');
   });
 
+  it("should insert values for tree with only one level", function(){
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(4);
+    expect(binarySearchTree.left.value).to.equal(4);
+    expect(binarySearchTree.right.value).to.equal(6);
+  });
+
   it("should insert values at the correct location in the tree", function(){
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
