a98b3f5479af839a695c764b7aab6546ad999f3a
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 20:39:36 -0800

    Initializes properties and methods for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,27 @@
 var makeBinarySearchTree = function(value){
+  var tree = {};
+
+  tree.right = null;
+  tree.left = null;
+  tree.value = value;
+  _.extend(tree, binarySearchTreeMethods);
+
+  return tree;
 };
+
+
+var binarySearchTreeMethods = {
+  insert: function() {
+
+  },
+
+  contains: function() {
+
+  },
+
+  depthFirstLog: function() {
+
+  }
+};
+
+
