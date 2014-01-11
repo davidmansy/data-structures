a62165caf17d7615a51e0e8dc8f99a33650c85d6
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 21:13:24 -0800

    Refactors insert for tree with no branches

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -13,11 +13,17 @@ var makeBinarySearchTree = function(value){
 var binarySearchTreeMethods = {
   insert: function(value) {
     var newTree = makeBinarySearchTree(value);
-    if(newTree.value > this.value) {
-      this.right = newTree; //
-    } else {
-      this.left = newTree;
-    } 
+
+    if(newTree.value > this.value) {  //insert somewhere to right
+      if(this.right === null) {
+        this.right = newTree;
+      }
+    } else { //insert somewhere to left
+      if(this.left === null) {
+        this.left = newTree;
+      }
+    }
+
   },
 
   contains: function() {
