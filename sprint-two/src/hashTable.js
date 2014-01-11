a71052c95c9b13712f47d09f852802ecb1b0d9a6
Hack Reactor Students <david.mansy@icloud.com>
2014-01-10 16:36:53 -0800

    Increases size of Hash Table storage when 75% of limit is reached

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,6 +6,24 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = this.getHash(k);
+
+  if(this._currentSize+1 >= this._limit * 0.75) {  //increase size of storage if current size is >=75% of size limit
+    var oldStorage = this._storage; //retain access to old storage
+    this._limit *= 2;  //double storage size limit
+    this._storage = makeLimitedArray(this._limit); //create new storage with larger size limit
+    this._currentSize = 0; //set current size to zero
+
+    //add key-value pairs into newly expanded storage
+    var hashTable = this;
+    oldStorage.each(function(bucket, index, storage) {
+      _.each(bucket, function(keyValuePair) {
+        hashTable.insert(keyValuePair[0], keyValuePair[1]);
+      });
+    });
+
+
+  }
+
   this._currentSize++;
   if(this._storage.get(i) === undefined) {
     this._storage.set(i, [[k,v]]);  //initialize array bucket with key-value array item
@@ -45,4 +63,3 @@ HashTable.prototype.remove = function(k){
 HashTable.prototype.getHash = function(k) {
   return getIndexBelowMaxForKey(k, this._limit);
 };
-
